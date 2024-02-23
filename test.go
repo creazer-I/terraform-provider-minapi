6c649ec0f9dcb2d8b75f05c275fff64ed094a759
Mahendra M <89565913+creazer-I@users.noreply.github.com>
2024-02-23 16:35:25 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,103 +1,99 @@
 package provider
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "io"
-    "net/http"
-    "strings"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
 
-    "github.com/hashicorp/terraform-plugin-framework/resource"
-    "github.com/hashicorp/terraform-plugin-framework/resource/schema"
-    "github.com/hashicorp/terraform-plugin-framework/tfsdk"
-    "github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ resource.Resource = (*MinAPIHttpResource)(nil)
 
 func NewMinAPIHttpResource() resource.Resource {
-    return &MinAPIHttpResource{}
+	return &MinAPIHttpResource{}
 }
 
 type MinAPIHttpResource struct{}
 
 func (r *MinAPIHttpResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-    resp.TypeName = "http"
+	resp.TypeName = "http"
 }
 
 func (r *MinAPIHttpResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
-    resp.Schema = schema.Schema{
-        Description: `
+	resp.Schema = schema.Schema{
+		Description: `
 The ` + "`http`" + ` resource makes an HTTP POST request to the given URL and exports
 information about the response.
 `,
-        Attributes: map[string]schema.Attribute{
-            "id": schema.StringAttribute{
-                Description: "The URL used for the request.",
-                Computed:    true,
-            },
-            "url": schema.StringAttribute{
-                Description: "The URL for the request.",
-                Required:    true,
-            },
-            "payload": schema.StringAttribute{
-                Description: "The payload to be sent in the POST request.",
-                Required:    true,
-                ValidateFunc: func(v interface{}, p string) (ws []string, errors []error) {
-                    // Validate payload length
-                    payload := v.(string)
-                    if len(payload) > 1000 {
-                        errors = append(errors, fmt.Errorf("payload must be at most 1000 characters long"))
-                    }
-                    return
-                },
-            },
-            "response_body": schema.StringAttribute{
-                Description: "The response body returned as a string.",
-                Computed:    true,
-            },
-        },
-    }
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description: "The URL used for the request.",
+				Computed:    true,
+			},
+			"url": schema.StringAttribute{
+				Description: "The URL for the request.",
+				Required:    true,
+			},
+			"payload": schema.StringAttribute{
+				Description: "The payload to be sent in the POST request.",
+				Required:    true,
+				ValidateFunc: func(v interface{}, c string) ([]string, []error) {
+					payload := v.(string)
+					if len(payload) > 1000 {
+						return nil, []error{fmt.Errorf("payload must be at most 1000 characters long")}
+					}
+					return nil, nil
+				},
+			},
+			"response_body": schema.StringAttribute{
+				Description: "The response body returned as a string.",
+				Computed:    true,
+			},
+		},
+	}
 }
 
 func (r *MinAPIHttpResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-    var config MinAPIHttpResourceModel
-    diags := req.Config.Get(ctx, &config)
-    resp.Diagnostics.Append(diags...)
-    if resp.Diagnostics.HasError() {
-        return
-    }
+	var config MinAPIHttpResourceModel
+	diags := req.Config.Get(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-    url := config.Url.ValueString()
-    payload := config.Payload.ValueString()
+	url := config.Url.ValueString()
+	payload := config.Payload.ValueString()
 
-    request, err := http.NewRequest("POST", url, strings.NewReader(payload))
-    if err != nil {
-        resp.Diagnostics.AddError("Error creating HTTP request", err.Error())
-        return
-    }
-    request.Header.Set("Content-Type", "application/json")
+	request, err := http.NewRequest("POST", url, strings.NewReader(payload))
+	if err != nil {
+		resp.Diagnostics.AddError("Error creating HTTP request", err.Error())
+		return
+	}
+	request.Header.Set("Content-Type", "application/json")
 
-    client := http.Client{}
-    response, err := client.Do(request)
-    if err != nil {
-        resp.Diagnostics.AddError("Error making HTTP request", err.Error())
-        return
-    }
-    defer response.Body.Close()
+	client := http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		resp.Diagnostics.AddError("Error making HTTP request", err.Error())
+		return
+	}
+	defer response.Body.Close()
 
-    responseBody, err := io.ReadAll(response.Body)
-    if err != nil {
-        resp.Diagnostics.AddError("Error reading response body", err.Error())
-        return
-    }
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		resp.Diagnostics.AddError("Error reading response body", err.Error())
+		return
+	}
 
-    resp.State.SetId(url)
-    resp.State.Set("response_body", types.StringValue(string(responseBody)))
+	resp.State.Set(ctx, "response_body", types.StringValue(string(responseBody)))
 }
 
 type MinAPIHttpResourceModel struct {
-    Url     types.String `tfsdk:"url"`
-    Payload types.String `tfsdk:"payload"`
+	Url     types.String `tfsdk:"url"`
+	Payload types.String `tfsdk:"payload"`
 }
